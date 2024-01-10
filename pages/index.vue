c3606390ebd8eb525442d0734d64269da7608c7f
NextFire <git@yuru.moe>
2024-01-09 19:28:30 -0500

    index: use hero

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <main class="h-screen p-8 flex flex-col justify-evenly items-center gap-8">
-    <h1 class="text-5xl text-center font-bold">
-      <span class="text-primary">nana</span>count
-    </h1>
-    <NuxtLink to="/new">
-      <button class="btn btn-lg btn-wide btn-primary">Start a new count</button>
-    </NuxtLink>
-  </main>
+  <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          <span class="text-primary">nana</span>count
+        </h1>
+        <p class="py-6">T**count but it's a webapp.</p>
+        <NuxtLink to="/new">
+          <a class="btn btn-lg btn-wide btn-primary">Start a new count</a>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>

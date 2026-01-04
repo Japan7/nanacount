b01ab6319d3c165d5d63468ba559fc42d3cc8d6f
NextFire <git@yuru.moe>
2026-01-05 00:47:56 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # nanacount
 
+```sh
+npm ci
+npx prisma migrate dev
+npm run dev
+```
+
 <img width="1440" alt="Screenshot 2024-01-11 at 00 08 53" src="https://github.com/Japan7/nanacount/assets/20094890/b43314e6-9552-4a00-9b6e-e55ada7f898b">
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: "2026-01-05",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@vueuse/nuxt"],
   app: {
